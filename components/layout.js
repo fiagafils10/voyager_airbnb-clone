75360c306bf72cd432beb81fd8e8f77440d6293c
Fiaga Fils <fiagafils@gmail.com>
2022-12-15 23:04:29 +0100

    initialize the first commit

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = ({children}) => {
+  return (
+    <div className='text-black font-bold'>
+        lorem20
+        {children}
+    </div>
+  )
+}
+
+export default Layout
